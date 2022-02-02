2eeb911f84f1414a1c591c4a2097b842fbb4b54d
rmichaud <rmichaud@icloud.com>
2022-02-01 22:42:49 -0500

    Initial Commit

---
+++ SystemBadge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SystemBadge
+//
+//  Created by Richard Michaud on 2/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SystemBadge/SystemBadgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemBadgeApp.swift
+//  SystemBadge
+//
+//  Created by Richard Michaud on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct SystemBadgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
